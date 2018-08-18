724660329007f5228580e03f5f574ffa761c262d
Guest <you@example.com>
2018-08-18 16:24:53 +0700

    .....

--- step_00/game/mouse.py
+++ step_00/game/mouse.py
@@ -4,47 +4,45 @@ import random
 from shoot import hit
 from pyglet.window import mouse
 
-
+batch = pyglet.graphics.Batch()
 window = pyglet.window.Window(800, 600)
 # window.push_handlers(pyglet.window.event.WindowEventLogger())
 label = pyglet.text.Label("0", x=0, y=0)
-gun = pyglet.resource.image("Limra-Morning-Walking-Stick-SDL023968542-1-b9b19.jpg")
-bird0 = pyglet.resource.image("Flappybirdlogo.jpg")
-# bird1 = pyglet.resource.image("Flappybirdlogo.jpg")
-bird2 = pyglet.resource.image("Flappybirdlogo.jpg")
+gun = pyglet.resource.image("battleback4.png")
+bird0 = pyglet.resource.image("plant1.png")
 gun.height = 400
 gun.width = 400
 bird0.height = 200
 bird0.width = 200
-img = bird0
 gun = pyglet.sprite.Sprite(gun, x=400, y=0)
-bird0_sprite = pyglet.sprite.Sprite(img, x=100, y=300)
-bird1 = pyglet.sprite.Sprite(img, x=400, y=300)
-bird2 = pyglet.sprite.Sprite(img, x=700, y=300)
+bird3 = pyglet.sprite.Sprite(img = bird0, x=700, y=300, batch=batch)
+bird1 = pyglet.sprite.Sprite(img = bird0, x=100, y=300, batch=batch)
+bird2 = pyglet.sprite.Sprite(img = bird0, x=400, y=300, batch=batch)
 gun.image.anchor_x = gun.width / 2
 gun.image.anchor_y = gun.height / 2
-bird0_sprite.image.anchor_x = bird0.width / 2
-bird0_sprite.image.anchor_y = bird0.height / 2
 bird1.image.anchor_x = bird1.width / 2
 bird1.image.anchor_y = bird1.height / 2
 bird2.image.anchor_x = bird2.width / 2
 bird2.image.anchor_y = bird2.height / 2
+bird3.image.anchor_x = bird3.width / 2
+bird3.image.anchor_y = bird3.height / 2
 gun.rotation = 0
-bird0_sprite.rotation = 0
+bird3.rotation = 0
 bird1.rotation = 0
 bird2.rotation = 0
 
-
+sprites = []
+sprites.append(bird1)
+sprites.append(bird2)
+sprites.append(bird3)
 # Event callbacks
 @window.event
 def on_draw():
     window.clear()
     # label.draw()
     gun.draw()
-    bird0_sprite.draw()
-    bird0_sprite.delete()
-    bird1.draw()
-    bird2.draw()
+    for a in sprites:
+        a.draw()
 
 
 @window.event
@@ -54,12 +52,11 @@ def on_mouse_motion(x, y, dx, dy):
 
 @window.event
 def on_mouse_press(x, y, button, modifier):
-    hit_bird = []
-    if button == mouse.LEFT:
-        bird_flock = [[100, 300], [400, 300], [700, 300]]
-        for chim in range(len(bird_flock)):
-            if hit(x, y, 100, 300) is True:
-                bird0_sprite.delete()
+    happy_tree_friends = [[100, 300], [400, 300], [700,300]]
+    for cay in range(len(happy_tree_friends)):
+        if button == mouse.LEFT:
+            if hit(x, y, happy_tree_friends[cay][0],  happy_tree_friends[cay][1]) is True:
+                sprites.remove(sprites[cay])
             else:
                 return False
 
