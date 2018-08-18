f310c7d7396424d8f9901aff1926c3653db09e95
Guest <you@example.com>
2018-08-18 22:38:29 +0700

    son commit

--- step_00/Game_almost_finished/MinionObjects.py
+++ step_00/Game_almost_finished/MinionObjects.py
@@ -2,6 +2,7 @@ import resources
 import pyglet
 from random import randint
 
+
 class Cowboy:
     def __init__(self, batch, image):
         self.posx = randint(0, 1200)
@@ -38,67 +39,37 @@ class Cowboy:
             self.cowboy_sprite.y += 50*dt
 
 
-
-
-class Witch(object):
-    def __init__(self, posx, posy, image=None):
-        self.posx = posx
-        self.posy = posy
+class Blob:
+    def __init__(self, batch, image):
+        self.posx = randint(0, 1200)
+        self.posy = randint(450, 950)
         self.velx = 0
         self.vely = 0
         if image is not None:
-            witch = pyglet.resource.image(image)
-            witch_seq = pyglet.image.ImageGrid(witch, 1 ,10 ,item_width = 200, item_height = 200)
-            witch_animation = pyglet.image.Animation.from_image_sequence(witch_seq[0:3], 0.1, loop=True)
-            self.witch_sprite = pyglet.sprite.Sprite(witch_animation, x=self.posx, y=self.posy)
+            blob = pyglet.resource.image(image)
+            blob_seq = pyglet.image.ImageGrid(blob, 1 ,8 ,item_width = 80, item_height = 80)
+            blob_animation = pyglet.image.Animation.from_image_sequence(blob_seq[0:7], 0.1, loop=True)
+            self.blob_sprite = pyglet.sprite.Sprite(blob_animation, x=self.posx, y=self.posy, batch=batch)
+
+            self.blob_sprite.image.anchor_x = 80 / 2
+            self.blob_sprite.image.anchor_y = 80 / 2
+            self.blob_sprite.rotation = 0
 
 
     def draw(self):
-        self.witch_sprite.draw()
+        self.blob_sprite.draw()
+
 
     def update(self, dt, pos):
-        print(pos)
-        if self.witch_sprite.x <= pos[0] and self.witch_sprite.y < pos[1]:
-            self.witch_sprite.x += 100*dt
-            self.witch_sprite.y += 100*dt
-        elif self.witch_sprite.x <= pos[0] and self.witch_sprite.y > pos[1]:
-            self.witch_sprite.x += 100*dt
-            self.witch_sprite.y -= 100*dt
-        elif self.witch_sprite.x >= pos[0] and self.witch_sprite.y > pos[1]:
-            self.witch_sprite.x -= 100*dt
-            self.witch_sprite.y -= 100*dt
+        if self.blob_sprite.x <= pos[0] and self.blob_sprite.y < pos[1]:
+            self.blob_sprite.x += 100*dt
+            self.blob_sprite.y += 100*dt
+        elif self.blob_sprite.x <= pos[0] and self.blob_sprite.y > pos[1]:
+            self.blob_sprite.x += 100*dt
+            self.blob_sprite.y -= 100*dt
+        elif self.blob_sprite.x >= pos[0] and self.blob_sprite.y > pos[1]:
+            self.blob_sprite.x -= 100*dt
+            self.blob_sprite.y -= 100*dt
         else:
-            self.witch_sprite.x -= 100*dt
-            self.witch_sprite.y += 100*dt
-
-
-class Plant(object):
-    def __init__(self, posx, posy, image=None):
-        self.posx = posx
-        self.posy = posy
-        self.velx = 0
-        self.vely = 0
-        if image is not None:
-            plant = pyglet.image.load("/tmp/guest-mo1w68/nson/son/res/sprites/" + image)
-            plant_seq = pyglet.image.ImageGrid(plant, 1 ,10 ,item_width = 200, item_height = 200)
-            plant_animation = pyglet.image.Animation.from_image_sequence(plant_seq[0:3], 0.1, loop=True)
-            self.plant_sprite = pyglet.sprite.Sprite(plant_animation, x=self.posx, y=self.posy)
-
-
-    def draw(self):
-        self.plant_sprite.draw()
-
-    def update(self, dt, pos):
-        print(pos)
-        if self.plant_sprite.x <= pos[0] and self.plant_sprite.y < pos[1]:
-            self.plant_sprite.x += 100*dt
-            self.plant_sprite.y += 100*dt
-        elif self.plant_sprite.x <= pos[0] and self.plant_sprite.y > pos[1]:
-            self.plant_sprite.x += 100*dt
-            self.plant_sprite.y -= 100*dt
-        elif self.plant_sprite.x >= pos[0] and self.plant_sprite.y > pos[1]:
-            self.plant_sprite.x -= 100*dt
-            self.plant_sprite.y -= 100*dt
-        else:
-            self.plant_sprite.x -= 100*dt
-            self.plant_sprite.y += 100*dt
+            self.blob_sprite.x -= 100*dt
+            self.blob_sprite.y += 100*dt
--- step_00/Game_almost_finished/shoot.py
+++ step_00/Game_almost_finished/shoot.py
@@ -1,11 +1,21 @@
 import pyglet
 
-def hit(x, y, position_x, position_y):
+
+def hit_cowboy(x, y, position_x, position_y):
     target_width = 45/2
     target_height = 45/2
     if range_x(position_x, target_width, x) is True and range_y(position_y, target_height, y) is True:
         return True
     return False
+
+
+def hit_blob(x, y, position_x, position_y):
+    target_width = 45
+    target_height = 45
+    if range_x(position_x, target_width, x) is True and range_y(position_y, target_height, y) is True:
+        return True
+    return False
+
 
 def range_x(position_x, width, x):
     return position_x - width <= x <= position_x + width
